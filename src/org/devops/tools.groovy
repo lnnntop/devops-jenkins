59423a9a55426a6ae86c8c6101cfea7517fc190c
lnnntop <42311069@smail.swufe.edu.cn>
2026-05-08 15:24:53 +0800

    Create tools.groovy

---
+++ src/org/devops/tools.groovy
@@ -0,0 +1,11 @@
+def printMsg(value,color){
+	colors = [
+		'red': "\033[40;31m >>>>>>${value}<<<<< \033[0m",
+		'blue':"\033[47;34m ${value} \033[0m",
+		'green':"\033[40;32m >>>>>>${value}<<<<< \033[0m",
+		'green1':"\033[40;32m >>>>>>${value}<<<<< \033[0m"
+	]
+	ansiColor('xterm'){
+		println(colors[color])
+	}
+}
